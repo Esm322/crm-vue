b43c04de8d501ba11d6ed1cccc18f199b47b3371
Esm322 <86346824+Esm322@users.noreply.github.com>
2024-04-09 21:15:08 +0300

    initial comp

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -95,188 +95,188 @@ import { useClientsStore } from '../stores/clientsData';
 import dateChange from '../helpers/dateChange';
 import dateCreation from '../helpers/dateCreation';
 
-export default {
-  data() {
-    return {
-      isModalAddVisible: false,
-      activeId: null,
-      activeIdDelete: null,
+// export default {
+//   data() {
+//     return {
+//       isModalAddVisible: false,
+//       activeId: null,
+//       activeIdDelete: null,
 
-      dirId: 0,
-      isDirId: false,
+//       dirId: 0,
+//       isDirId: false,
 
-      dirFullName: 0,
-      isDirFullName: false,
+//       dirFullName: 0,
+//       isDirFullName: false,
 
-      dirCreate: 0,
-      isDirCreate: false,
+//       dirCreate: 0,
+//       isDirCreate: false,
 
-      dirEdit: 0,
-      isDirEdit: false,
-    };
-  },
-  components: {
-    ModalWindowAdd,
-    ModalWindowChanges,
-    ModalWindowDelete,
-    ClientTableTbody,
-    DownArrowSVG,
-    UpArrowSVG,
-    AlphabetSVG,
-    AddSVG,
-  },
-  computed: {
-    ...mapState(useClientsStore, ['clientsData', 'searchValueStore']),
-    clients() {
-      return this.getClientsData ? this.getClientsData.map((client) => {
-        return {
-          ...client,
-          id: client.id,
-          fullName: `${client.secondName} ${client.firstName} ${client.thirdName}`,
-          date: dateCreation(client.date.newDate),
-          edit: dateChange(client.edit.newEdit),
-        };
-      }) : [];
-    },
-    getClientsData() {
-      let copyData = this.clientsData;
+//       dirEdit: 0,
+//       isDirEdit: false,
+//     };
+//   },
+//   components: {
+//     ModalWindowAdd,
+//     ModalWindowChanges,
+//     ModalWindowDelete,
+//     ClientTableTbody,
+//     DownArrowSVG,
+//     UpArrowSVG,
+//     AlphabetSVG,
+//     AddSVG,
+//   },
+//   computed: {
+//     ...mapState(useClientsStore, ['clientsData', 'searchValueStore']),
+//     clients() {
+//       return this.getClientsData ? this.getClientsData.map((client) => {
+//         return {
+//           ...client,
+//           id: client.id,
+//           fullName: `${client.secondName} ${client.firstName} ${client.thirdName}`,
+//           date: dateCreation(client.date.newDate),
+//           edit: dateChange(client.edit.newEdit),
+//         };
+//       }) : [];
+//     },
+//     getClientsData() {
+//       let copyData = this.clientsData;
 
-      if (this.dirId === 1 && this.isDirId === true) {
-        copyData = copyData
-          .sort((clientA, clientB) => clientA.id - clientB.id);
-      } else if (this.dirId === 2 && this.isDirId === true) {
-        copyData = copyData
-          .sort((clientA, clientB) => clientB.id - clientA.id);
-      }
+//       if (this.dirId === 1 && this.isDirId === true) {
+//         copyData = copyData
+//           .sort((clientA, clientB) => clientA.id - clientB.id);
+//       } else if (this.dirId === 2 && this.isDirId === true) {
+//         copyData = copyData
+//           .sort((clientA, clientB) => clientB.id - clientA.id);
+//       }
 
-      if (this.isDirFullName === true && this.dirFullName === 1) {
-        copyData = copyData
-          .sort((clientA, clientB) => (clientA.secondName < clientB.secondName ? -1 : 1));
-      } else if (this.isDirFullName === true && this.dirFullName === 2) {
-        copyData = copyData
-          .sort((clientA, clientB) => (clientA.secondName > clientB.secondName ? -1 : 1));
-      }
+//       if (this.isDirFullName === true && this.dirFullName === 1) {
+//         copyData = copyData
+//           .sort((clientA, clientB) => (clientA.secondName < clientB.secondName ? -1 : 1));
+//       } else if (this.isDirFullName === true && this.dirFullName === 2) {
+//         copyData = copyData
+//           .sort((clientA, clientB) => (clientA.secondName > clientB.secondName ? -1 : 1));
+//       }
 
-      if (this.dirCreate === 1 && this.isDirCreate === true) {
-        copyData = copyData
-          .sort((clientA, clientB) => clientA.date.nowDate - clientB.date.nowDate);
-      } else if (this.dirCreate === 2 && this.isDirCreate === true) {
-        copyData = copyData
-          .sort((clientA, clientB) => clientB.date.nowDate - clientA.date.nowDate);
-      }
+//       if (this.dirCreate === 1 && this.isDirCreate === true) {
+//         copyData = copyData
+//           .sort((clientA, clientB) => clientA.date.nowDate - clientB.date.nowDate);
+//       } else if (this.dirCreate === 2 && this.isDirCreate === true) {
+//         copyData = copyData
+//           .sort((clientA, clientB) => clientB.date.nowDate - clientA.date.nowDate);
+//       }
 
-      if (this.dirEdit === 1 && this.isDirEdit === true) {
-        copyData = copyData
-          .sort((clientA, clientB) => clientA.edit.nowEdit - clientB.edit.nowEdit);
-      } else if (this.dirEdit === 2 && this.isDirEdit === true) {
-        copyData = copyData
-          .sort((clientA, clientB) => clientB.edit.nowEdit - clientA.edit.nowEdit);
-      }
+//       if (this.dirEdit === 1 && this.isDirEdit === true) {
+//         copyData = copyData
+//           .sort((clientA, clientB) => clientA.edit.nowEdit - clientB.edit.nowEdit);
+//       } else if (this.dirEdit === 2 && this.isDirEdit === true) {
+//         copyData = copyData
+//           .sort((clientA, clientB) => clientB.edit.nowEdit - clientA.edit.nowEdit);
+//       }
 
-      if (this.searchValueStore) {
-        copyData = copyData
-          .filter((client) => {
-            return Object.values(client)
-              .some((item) => item
-                .toString().toLowerCase().includes(this.searchValueStore.toLowerCase()));
-          });
-      }
+//       if (this.searchValueStore) {
+//         copyData = copyData
+//           .filter((client) => {
+//             return Object.values(client)
+//               .some((item) => item
+//                 .toString().toLowerCase().includes(this.searchValueStore.toLowerCase()));
+//           });
+//       }
 
-      return copyData;
-    },
-  },
-  methods: {
-    isSortId() {
-      this.dirFullName = 0;
-      this.isDirFullName = false;
+//       return copyData;
+//     },
+//   },
+//   methods: {
+//     isSortId() {
+//       this.dirFullName = 0;
+//       this.isDirFullName = false;
 
-      this.dirCreate = 0;
-      this.isDirCreate = false;
+//       this.dirCreate = 0;
+//       this.isDirCreate = false;
 
-      this.dirEdit = 0;
-      this.isDirEdit = false;
+//       this.dirEdit = 0;
+//       this.isDirEdit = false;
 
-      if (this.dirId === 0 && this.isDirId === false) {
-        this.dirId = 1;
-        this.isDirId = true;
-      } else if (this.dirId === 1 && this.isDirId === true) {
-        this.dirId = 2;
-      } else {
-        this.dirId = 1;
-      }
-    },
-    isSortFullName() {
-      this.dirId = 0;
-      this.isDirId = false;
+//       if (this.dirId === 0 && this.isDirId === false) {
+//         this.dirId = 1;
+//         this.isDirId = true;
+//       } else if (this.dirId === 1 && this.isDirId === true) {
+//         this.dirId = 2;
+//       } else {
+//         this.dirId = 1;
+//       }
+//     },
+//     isSortFullName() {
+//       this.dirId = 0;
+//       this.isDirId = false;
 
-      this.dirCreat = 0;
-      this.isDirCreate = false;
+//       this.dirCreat = 0;
+//       this.isDirCreate = false;
 
-      this.dirEdit = 0;
-      this.isDirEdit = false;
+//       this.dirEdit = 0;
+//       this.isDirEdit = false;
 
-      if (this.isDirFullName === false && this.dirFullName === 0) {
-        this.isDirFullName = true;
-        this.dirFullName = 1;
-      } else if (this.isDirFullName === true && this.dirFullName === 1) {
-        this.dirFullName = 2;
-      } else {
-        this.dirFullName = 1;
-      }
-    },
-    isSortCreate() {
-      this.dirId = 0;
-      this.isDirId = false;
+//       if (this.isDirFullName === false && this.dirFullName === 0) {
+//         this.isDirFullName = true;
+//         this.dirFullName = 1;
+//       } else if (this.isDirFullName === true && this.dirFullName === 1) {
+//         this.dirFullName = 2;
+//       } else {
+//         this.dirFullName = 1;
+//       }
+//     },
+//     isSortCreate() {
+//       this.dirId = 0;
+//       this.isDirId = false;
 
-      this.dirFullName = 0;
-      this.isdirFullName = false;
+//       this.dirFullName = 0;
+//       this.isdirFullName = false;
 
-      this.dirEdit = 0;
-      this.isDirEdit = false;
+//       this.dirEdit = 0;
+//       this.isDirEdit = false;
 
-      if (this.dirCreate === 0 && this.isDirCreate === false) {
-        this.dirCreate = 1;
-        this.isDirCreate = true;
-      } else if (this.dirCreate === 1 && this.isDirCreate === true) {
-        this.dirCreate = 2;
-      } else {
-        this.dirCreate = 1;
-      }
-    },
-    isSortEdit() {
-      this.dirId = 0;
-      this.isDirId = false;
+//       if (this.dirCreate === 0 && this.isDirCreate === false) {
+//         this.dirCreate = 1;
+//         this.isDirCreate = true;
+//       } else if (this.dirCreate === 1 && this.isDirCreate === true) {
+//         this.dirCreate = 2;
+//       } else {
+//         this.dirCreate = 1;
+//       }
+//     },
+//     isSortEdit() {
+//       this.dirId = 0;
+//       this.isDirId = false;
 
-      this.dirFullName = 0;
-      this.isdirFullName = false;
+//       this.dirFullName = 0;
+//       this.isdirFullName = false;
 
-      this.dirCreate = 0;
-      this.isDirCreate = false;
+//       this.dirCreate = 0;
+//       this.isDirCreate = false;
 
-      if (this.dirEdit === 0 && this.isDirEdit === false) {
-        this.dirEdit = 1;
-        this.isDirEdit = true;
-      } else if (this.dirEdit === 1 && this.isDirEdit === true) {
-        this.dirEdit = 2;
-      } else {
-        this.dirEdit = 1;
-      }
-    },
-    showModalAdd() {
-      this.isModalAddVisible = true;
-    },
-    closeModal() {
-      this.activeId = null;
-      this.isModalAddVisible = false;
-      this.activeIdDelete = null;
-    },
-  },
-  mounted() {
-    this.$watch(
-      () => this.isModalAddVisible || this.activeId || this.activeIdDelete,
-      (val) => document.body.classList.toggle('no-overflow', val),
-      { immediate: true },
-    );
-  },
-};
+//       if (this.dirEdit === 0 && this.isDirEdit === false) {
+//         this.dirEdit = 1;
+//         this.isDirEdit = true;
+//       } else if (this.dirEdit === 1 && this.isDirEdit === true) {
+//         this.dirEdit = 2;
+//       } else {
+//         this.dirEdit = 1;
+//       }
+//     },
+//     showModalAdd() {
+//       this.isModalAddVisible = true;
+//     },
+//     closeModal() {
+//       this.activeId = null;
+//       this.isModalAddVisible = false;
+//       this.activeIdDelete = null;
+//     },
+//   },
+//   mounted() {
+//     this.$watch(
+//       () => this.isModalAddVisible || this.activeId || this.activeIdDelete,
+//       (val) => document.body.classList.toggle('no-overflow', val),
+//       { immediate: true },
+//     );
+//   },
+// };
 </script>
